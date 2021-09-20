e317d6d6ed73f5e4017edad2cdbc1cccebf35c8a
bernardo mendonça <bernardomendonca@outlook.com>
2021-09-20 16:29:06 +1000

    Boilerplate setup

---
+++ test/Inbox.test.js
@@ -0,0 +1,8 @@
+// contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+//creating an instance of Web3
+const web3 = new Web3(ganache.provider());
+
